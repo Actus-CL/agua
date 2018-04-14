729ed5ffff5064db606ac48c6aa05f3bd68921b1
Actus <rinostrozareb@gmail.com>
2018-04-14 16:35:25 -0300

    Modelos

---
+++ app/Models/Alerta/AlertaEntregaSistema.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models\Alerta;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+
+class AlertaEntregaCorreo extends Model
+{
+    protected $table = 'alerta_entrega_correo';
+
+    /*
+    public function proyectos()
+    {
+        return $this->belongsToMany('App\proyecto' ,'cliente_proyecto');
+    }
+
+    public function nombreCompleto()
+    {
+        return $this->nombre .' '. $this->apellido_paterno .' '. $this->apellido_materno;
+    }
+
+    public function cuentas()
+    {
+        return $this->hasMany('App\Cuenta' ,'cliente_id');
+    }
+
+    public function boletas()
+    {
+        return $this->hasMany('App\Boleta' ,'cliente_id');
+    }
+    public function monto_adeudado()
+    {
+        $monto_total=0;
+        $montodb= DB::select('select SUM(total) as monto_total from boleta where estado_pago_id <> 3 and cliente_id = ?', [$this->id]);
+        if($montodb>0){
+            $monto_total=$montodb[0]->monto_total;
+        }
+        return $monto_total;
+    }
+    */
+}
