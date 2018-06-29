ce732e6749a58b42ae15979d74399cde0b475189
Ricardo Inostoza Rebolledo <rinostrozareb@gmail.com>
2018-06-29 03:15:59 -0400

    d

--- app/Http/Controllers/MembershipController.php
+++ app/Http/Controllers/MembershipController.php
@@ -39,6 +39,7 @@ class MembershipController extends Controller
             $membership->put('shopUrl', $protectionShopToken->shop_url);
         }
         $cliente=Cliente::where("user_id",$user->id);
+        dd($cliente);
         $periodo_lec=Periodo::where("activo_lectura",1)->first();
         $periodos=Periodo::where("id","<=",$periodo_lec->id)->get()->sortByDesc('id')->take(6)->reverse();
 
